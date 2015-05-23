55a692fff7474880a61eac186b8005ff1dbd037d
Simon Leigh <simon.leigh@couchbase.com>
2015-05-23 22:40:11 +0100

    Added method in app.js to call the findEvent rest endpoint with the bounds of the map

--- app/app.js
+++ app/app.js
@@ -3,76 +3,89 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', ['uiGmapgoogle-maps'])
     .config(['uiGmapGoogleMapApiProvider'
-      , function(GoogleMapApi) {
-        GoogleMapApi.configure({
-          key: 'AIzaSyDxhBvEAPgJNWhR-UbnDEusPT29neQkV0k',
-          v: '3.17',
-          libraries: 'weather,geometry,visualization'
-        });
-    }])
+        , function(GoogleMapApi) {
+            GoogleMapApi.configure({
+                key: 'AIzaSyDxhBvEAPgJNWhR-UbnDEusPT29neQkV0k',
+                v: '3.17',
+                libraries: 'weather,geometry,visualization'
+            });
+        }])
 
     .controller('controlController', function ($scope) {
-      $scope.controlText = 'I\'m a custom control';
-      $scope.danger = false;
-      $scope.controlClick = function () {
-        $scope.danger = !$scope.danger;
-        alert('custom control clicked!')
-      };
+        $scope.controlText = 'I\'m a custom control';
+        $scope.danger = false;
+        $scope.controlClick = function () {
+            $scope.danger = !$scope.danger;
+            alert('custom control clicked!')
+        };
     })
 
     .controller("myAppCtrl", ['$scope','$http','uiGmapGoogleMapApi'
-      , function($scope, $http, GoogleMapApi) {
+        , function($scope, $http, GoogleMapApi) {
 
-      // Do this here to ensure that the maps API is loaded before we do anything else
-      GoogleMapApi.then(function(maps) {
-        $scope.googleVersion = maps.version;
-        maps.visualRefresh = true;
+            // Do this here to ensure that the maps API is loaded before we do anything else
+            GoogleMapApi.then(function(maps) {
+                $scope.googleVersion = maps.version;
+                maps.visualRefresh = true;
 
-        angular.extend($scope, {
-          map: {
-            show: true,
-            control: {},
-            version: "unknown",
-          showTraffic: true,
-          showBicycling: false,
-          showWeather: false,
-          center: {
-            latitude: 45,
-            longitude: -73
-          },
-          options: {
-            streetViewControl: false,
-            panControl: false,
-            maxZoom: 20,
-            minZoom: 3
-          },
-          zoom: 3,
-          dragging: false,
-          bounds: {}
-        }
-        });
+                angular.extend($scope, {
+                    map: {
+                        show: true,
+                        control: {},
+                        version: "unknown",
+                        showTraffic: true,
+                        showBicycling: false,
+                        showWeather: false,
+                        center: {
+                            latitude: 45,
+                            longitude: -73
+                        },
+                        options: {
+                            streetViewControl: false,
+                            panControl: false,
+                            maxZoom: 20,
+                            minZoom: 3
+                        },
+                        zoom: 3,
+                        dragging: false,
+                        bounds: {}
+                    }
+                });
 
-        $scope.refreshMap = function () {
-          //optional param if you want to refresh you can pass null undefined or false or empty arg
-          $scope.map.control.refresh({latitude: 51.5227, longitude: -0.0845});
-          $scope.map.control.getGMap().setZoom(16);
-          return;
-        };
+                $scope.refreshMap = function () {
+                    //optional param if you want to refresh you can pass null undefined or false or empty arg
+                    $scope.map.control.refresh({latitude: 51.5227, longitude: -0.0845});
+                    $scope.map.control.getGMap().setZoom(16);
+                    return;
+                };
 
-        $scope.getMapInstance = function () {
-          alert("You have Map Instance of" + $scope.map.control.getGMap().toString());
-          return;
-        };
+                $scope.getMapInstance = function () {
+                    alert("You have Map Instance of" + $scope.map.control.getGMap().toString());
+                    return;
+                };
+
+                $scope.findEvents=function(){
+                    return $http.get("/api/events/findEvents",{
+                        params:{
+                            ne_lat: map.bounds.northeast.latitude,
+                            ne_lon: map.bounds.northeast.longitude,
+                            sw_lat: map.bounds.southwest.latitude,
+                            sw_lon: map.bounds.southwest.longitude
+                        }
+                    }).then(function(response){
+                        return response.data;
+                    });
+                }
 
 
-        var versionUrl = (window.location.host === "rawgithub.com" || window.location.host === "rawgit.com") ?
-            "../package.json" : "/package.json";
+                var versionUrl = (window.location.host === "rawgithub.com" || window.location.host === "rawgit.com") ?
+                    "../package.json" : "/package.json";
 
-        $http.get(versionUrl).success(function (data) {
-          if (!data)
-            console.error("no version object found!!");
-          $scope.version = data.version;
-        });
-      });
+                $http.get(versionUrl).success(function (data) {
+                    if (!data)
+                        console.error("no version object found!!");
+                    $scope.version = data.version;
+                });
+            });
 
-    }]);
+        }]);
